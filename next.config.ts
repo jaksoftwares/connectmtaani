672773368d5bb0cada13633cc2ac6917281cf447
jaksoftwares <jaksoftwares05@gmail.com>
2025-03-05 13:21:40 +0300

    fix: :bug: addition of the dist in nect.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['via.placeholder.com'], 
+    domains: ["via.placeholder.com"],
   },
+  output: "export",
+  distDir: "out", 
 };
 
 export default nextConfig;
